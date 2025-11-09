e0ff0ad1968d7a81274f4c32151339f489744928
Santi <gugoansa.au@gmail.com>
2025-11-10 00:31:51 +1100

    last changes CI-CD

--- playwright.config.js
+++ playwright.config.js
@@ -29,7 +29,7 @@ export default defineConfig({
   reporter: [
     ['list'], // opcional, para ver en consola
     ['html', { outputFolder: 'playwright-report', open: 'never' }],
-    ['junit', { outputFile: 'results.xml' }]
+    ['junit', { outputFile: 'playwright-report/results.xml' }]
   ],
    /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
